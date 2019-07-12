c3fd2810c772368cf64681c6aec1e29f3c1b05a7
David M <dav2113379@gmail.com>
2019-07-11 22:27:01 -0700

    removed build from gitignore

---
+++ build/precache-manifest.a5bb191af90329461af4963fd8777944.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5faaa058b030809014a1428cb149faf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56761b86db424c319715",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "3a8cc4d7bf37e66324e4",
+    "url": "/static/js/2.d68f1bfa.chunk.js"
+  },
+  {
+    "revision": "56761b86db424c319715",
+    "url": "/static/js/main.84201c56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
